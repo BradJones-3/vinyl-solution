1b1a4d7ef8a3493a1df925e3d3dd623432b54a43
BradJones-3 <bradjones_95@hotmail.com>
2022-06-15 18:34:32 +0000

    altered CSS/added sizes to product model and size selector box to template

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
     """ Shows Users bag contents and also works out delivery charges on items in bag """
 
