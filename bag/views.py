fcfea4a027c3ebfe6fb1f27372398c127109c811
BradJones-3 <bradjones_95@hotmail.com>
2022-06-18 16:14:04 +0000

    began adding toast messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Allows users to add items to bag"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -31,6 +34,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -83,3 +87,4 @@ def remove_from_bag(request, item_id):
 
     except Exception as e:
         return HttpResponse(status=500)
+    print(console.log)
