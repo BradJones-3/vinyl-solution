a79940a2d2f50fc35dacc33da6a2d24374f54589
BradJones-3 <bradjones_95@hotmail.com>
2022-06-20 08:09:39 +0000

    moved toasts to templates folder and styled messages in CSS

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
 
@@ -13,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Allows users to add items to bag"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -26,7 +26,7 @@ def add_to_bag(request, item_id):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
                 messages.success(request,
-                                 f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]}')        
+                                 f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]}["items_by_size"][size]')        
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request,
@@ -39,8 +39,7 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
             messages.success(request,
-                             f'Updated
-                             { product.name } quantity to {bag[item_id]}')
+                             f'Updated { product.name } quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request,
@@ -53,6 +52,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Allows users to change item quantity inside the bag view"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
@@ -62,15 +62,21 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request,
+                             f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]}["items_by_size"][size]')    
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop()
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request,
+                             f'Updated { product.name } quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag.')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -80,6 +86,7 @@ def remove_from_bag(request, item_id):
     """ Allows users to remove items from their bag"""
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -89,12 +96,15 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag.')
         else:
             bag.pop(item_id)
+            messages.success((request), f'Removed {product.name} from your bag.')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: (e)')
         return HttpResponse(status=500)
     print(console.log)
