1e8795f461d6a7108c34f7897882a54b2ffb450d
BradJones-3 <bradjones_95@hotmail.com>
2022-06-25 15:03:09 +0000

    added stripe js

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_live_51KUDkJGqe9QmhagcSovqJmWseyb5k37iQtnA2mKP00Czy3Oj8ZGRzZJpc8HE1Z0Mskts6oNQbczQZ7XZf3TbgtiC00zECeKB63',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
