5d7c3ecf3ff1eb03ec108a6e49a2078fb54791d8
BradJones-3 <bradjones_95@hotmail.com>
2022-06-25 18:11:47 +0000

    added more for striple payments

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,5 +2,20 @@ var stripe_public_key = $('#id_stripe_public_key').text.slice(1,-1);
 var client_secret = $('#id_client_secret').text.slice(1,-1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#000'
+        }
+    },
+    invalid: {
+        color: '#F39C12',
+        iconColor: '#F39C12'
+    }
+};
 var card =elements.create(card);
-card.mount('#card-element');
+card.mount('#card-element', {style: style});
