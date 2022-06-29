185c25b6d56078dcd08c7e49846734ea44e7f0b6
BradJones-3 <bradjones_95@hotmail.com>
2022-06-29 20:46:26 +0000

    setting up heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vinyl-solution.wsgi:application
--- vinyl_solution/settings.py
+++ vinyl_solution/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j5i%dhjb(b$*9p$!vxhy$2xcfr_=_6a$0_s_(*ff*e@l7^42bm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,13 +120,16 @@ WSGI_APPLICATION = 'vinyl_solution.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:#
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
