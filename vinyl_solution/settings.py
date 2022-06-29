0bf614ccff27c5d4e5065936991f2868d5be9065
BradJones-3 <bradjones_95@hotmail.com>
2022-06-29 21:48:14 +0000

    removed secret key and set debug

--- vinyl_solution/settings.py
+++ vinyl_solution/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j5i%dhjb(b$*9p$!vxhy$2xcfr_=_6a$0_s_(*ff*e@l7^42bm'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['vinyl-solution.herokuapp.com', 'localhost']
 
