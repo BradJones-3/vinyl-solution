fca2b32800c2be730aceaea67b48a3216593b7e2
BradJones-3 <bradjones_95@hotmail.com>
2022-08-19 19:32:56 +0000

    added detail view and url

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import BlogPost
+from django.shortcuts import render, get_object_or_404
+
+from .models import BlogPost, BlogComment
 
 
 def blog(request):
@@ -12,3 +13,17 @@ def blog(request):
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def blog_detail(request, blogpost_id):
+    """ A view to display blog detail page """
+    
+    blogpost = get_object_or_404(BlogPost, pk=blogpost_id)
+    blog_comments = BlogComment.objects.filter(blogpost=blogpost)
+
+    context = {
+        'blogpost': blogpost,
+        'blogcomments': blogcomments,
+    }
+
+    return render(request, 'blog/blog_detail.html', context)
