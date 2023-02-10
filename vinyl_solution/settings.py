1220ce8b5173913c1163318b5d31d420e073b6de
BradJones-3 <bradjones_95@hotmail.com>
2023-02-10 13:11:14 +0000

    trying to connect db to elephantsql

--- vinyl_solution/settings.py
+++ vinyl_solution/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['vinyl-solution.herokuapp.com', 'localhost', 'vinyl-solution.onrender']
+ALLOWED_HOSTS = ['vinyl-solution.herokuapp.com', 'localhost',
+                 'vinyl-solution.onrender']
 
 
 # Application definition
